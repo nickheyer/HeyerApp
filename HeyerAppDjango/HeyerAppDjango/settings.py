37d9d74a2265e72060c36542ec8d1f5ad1d9d14a
nickheyer <nick@heyer.app>
2022-09-06 23:01:36 -0700

    User Agents

--- HeyerAppDjango/HeyerAppDjango/settings.py
+++ HeyerAppDjango/HeyerAppDjango/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_user_agents',
     'Portfolio',
 ]
 
@@ -69,12 +70,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-MIDDLEWARE_CLASSES = (
-    # other middlewares...
     'django_user_agents.middleware.UserAgentMiddleware',
-)
+]
 
 ROOT_URLCONF = 'HeyerAppDjango.urls'
 
