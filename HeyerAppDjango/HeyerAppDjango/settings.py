60d5b28572f86f632ba17986c8e5ac4e13aecf03
nickheyer <nick@heyer.app>
2022-10-01 12:29:03 -0700

    Trying custom dir

--- HeyerAppDjango/HeyerAppDjango/settings.py
+++ HeyerAppDjango/HeyerAppDjango/settings.py
@@ -31,8 +31,6 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
