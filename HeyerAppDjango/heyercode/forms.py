7a7b9fa08e2ce6b7982d5df43e3f576af659b928
nickheyer <nick@heyer.app>
2022-10-12 22:57:58 -0700

    Added description to /heyercode

--- HeyerAppDjango/heyercode/forms.py
+++ HeyerAppDjango/heyercode/forms.py
@@ -2,8 +2,3 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.widgets import CheckboxSelectMultiple
-
-class UserCreateForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
